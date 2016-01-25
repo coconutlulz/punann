d22b19c0fc3f9a4f01cee914a7a548deb9be91d0
David Ó Laıġeanáın <david.lynam@redbrick.dcu.ie>
2016-01-25 14:38:36 +0000

    New menu and name services. Support for ui-router.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
 (function() {
-    angular.module('punann', ['ngAnimate'])
-        .config(['$logProvider', function($logProvider) {
+    angular.module('punann', ['ngAnimate', 'ui.router'])
+        .config(['$logProvider', '$stateProvider', '$urlRouterProvider', function($logProvider, $stateProvider, $urlRouterProvider) {
             $logProvider.debugEnabled(true);
+
+            $urlRouterProvider.otherwise('/home');
+
+            $stateProvider
+                .state('home', {
+                    url: '/home',
+                    templateUrl: 'templates/partial-home.html'
+                })
+                .state('home.list', {
+                    url: '/list',
+                    templateUrl: 'templates/partial-home-list.html',
+                    controller: function($scope) {
+                        $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+                    }
+                })
+                .state('home.paragraph', {
+                    url: '/paragraph',
+                    template: 'Booze me.'
+                })
+                .state('education', {
+                    url: '/education',
+                    views: {
+                        '': { templateUrl: 'templates/partial-education.html' },
+
+                        'formal@about': { template: 'aaaahl' },
+
+                        'informal@about': { template: 'Look! I am a column!' }
+                    }
+                });
         }]);
 })();
--- js/controllers/menu.js
+++ js/controllers/menu.js
@@ -1,44 +1,133 @@
 (function() {
     angular.module('punann')
-        .controller('NameController', ['$interval', function($interval) {
-            var index = 0;
-            var surnames = [
+        .service('nameService', function() {
+            this.firstName = 'David';
+            this.fade = false;
+
+            this.nameIndex = 0;
+            this.surnames = [
                 'Lynam',
                 'Ó Laıġeanáın'
             ];
 
-            this.firstName = 'David';
-            this.fade = false;
-            this.currentSurname = surnames[index];
-            this.nextSurname = surnames[(index + 1) % surnames.length];
-            var controller = this;
+            this.getFirstName = function() {
+                return this.firstName;
+            };
 
-            function fadeNextName() {
-                controller.fade = !controller.fade;
+            this.getCurrentSurname = function() {
+                return this.surnames[this.nameIndex];
+            };
 
-                index = (index + 1) % surnames.length;
+            this.getNextSurname = function() {
+                return this.surnames[(this.nameIndex + 1) % this.surnames.length];
+            };
 
-                controller.currentSurname = surnames[index];
-                controller.nextSurname = surnames[(index + 1) % surnames.length];
-            }
+            this.getFade = function() {
+                return this.fade;
+            };
 
-            $interval(fadeNextName, 5000);
-        }])
-        .controller('MenuController', function() {
+            this.setCurrentSurname = function(surname) {
+                this.currentSurname = surname;
+            };
+
+            this.setNextSurname = function(surname) {
+                this.nextSurname = surname;
+            };
+
+            this.shiftNameIndex = function() {
+                this.nameIndex = (this.nameIndex + 1) % this.surnames.length;
+            };
+
+            this.toggleFade = function() {
+                this.fade = !this.fade;
+            };
+        })
+        .service('menuService', function() {
             this.languages = ['english', 'irish'];
             this.currentLanguage = 0;
 
-            this.menuItems = {}
-            this.menuItems.english = [
+            this.menu = {};
+            this.menu.english = [
                 'Synopsis',
                 'Interests',
                 'Experience',
                 'Education'];
-            this.menuItems.irish = [
+
+            this.menu.irish = [
                  'Aċoimre',
                  'Caiṫeaṁ Aimsire',
                  'Taiṫí',
                  'Oideaċas'];
+
+            this.menu.links = [
+                '.synopsis',
+                '.interests',
+                '.experience',
+                '.education'];
             
+            var service = this;
+
+            this.getCurrentLanguage = function() {
+                return service.languages[service.currentLanguage];
+            };
+
+            this.getMenuLabelsForCurrentLanguage = function() {
+                return service.menu[service.getCurrentLanguage()];
+            };
+
+            this.getMenuLinks = function() {
+                return service.menu.links;
+            };
+
+            this.setCurrentLanguage = function(index) {
+                service.currentLanguage = index;
+            };
+
         })
+        .controller('NameController', ['$interval', 'nameService', function($interval, nameService) {
+            this.firstName = nameService.getFirstName();
+
+            this.getCurrentSurname = function() {
+                return nameService.getCurrentSurname();
+            };
+
+            this.getFade = function() {
+                return nameService.getFade();
+            };
+
+            function fadeNextName() {
+                nameService.toggleFade();
+                nameService.shiftNameIndex();
+            }
+
+            $interval(fadeNextName, 5000);
+        }])
+        .controller('MenuController', ['menuService', function(menuService) {
+            this.currentMenu = [];
+            var controller = this;
+
+            var rebuildMenu = function() {
+                var newMenu = [];
+                var labels = menuService.getMenuLabelsForCurrentLanguage();
+                var links = menuService.getMenuLinks();
+
+                for(var i = 0; i < labels.length; i++) {
+                    var newItem = {};
+                    newItem.label = labels[i];
+                    newItem.link = links[i];
+                    newMenu[i] = newItem;
+                }
+
+                controller.currentMenu = newMenu;
+            }
+
+            rebuildMenu();
+
+            var changeLanguage = function(index) {
+                if(menuService.getCurrentLanguage != index) {
+                    menuService.setCurrentLanguage(index);
+                    controller.rebuildMenu();
+                }
+            }
+        }])
 })();
