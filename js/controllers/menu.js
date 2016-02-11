101929e8e7a89da15496ce1e0e6af6811d6e9e01
David Ó Laıġeanáın <david.lynam@redbrick.dcu.ie>
2016-02-11 13:32:17 +0000

    Completely move menu items into menu service. Replace in controller with getItems().

--- js/controllers/menu.js
+++ js/controllers/menu.js
@@ -1,31 +1,8 @@
 (function() {
     angular.module('punann')
        .controller('MenuController', ['$translate', 'menuService', function($translate, menuService) {
-            this.menu = [
-                {
-                    id: 'SYNOPSIS',
-                    link: '.synopsis'
-                },
-                {
-                    id: 'INTERESTS',
-                    link: '.interests'
-                },
-                {
-                    id: 'EXPERIENCE',
-                    link: '.experience'
-                },
-                {
-                    id: 'EDUCATION',
-                    link: '.education'
-                },
-                {
-                    id: 'CONTACT',
-                    link: '.contact'
-                },
-                {
-                    id: 'BLOG',
-                    link: '.blog'
-                }
-            ];
+           this.getItems = function() {
+               return menuService.menu;
+           };
         }])
 })();
