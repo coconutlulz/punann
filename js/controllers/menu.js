f6b570035ffe307fd755ee8f495685f9b9ebf185
David Ó Laıġeanáın <david.lynam@redbrick.dcu.ie>
2016-03-02 13:30:47 +0000

    Navbar highlighting for selected page.

--- js/controllers/menu.js
+++ js/controllers/menu.js
@@ -1,8 +1,13 @@
 (function() {
     angular.module('punann')
-       .controller('MenuController', ['$translate', 'menuService', function($translate, menuService) {
+       .controller('MenuController', ['$translate', '$location', 'menuService', function($translate, $location, menuService) {
            this.getItems = function() {
                return menuService.menu;
            };
+
+           this.checkSelected = function(page) {
+               var selectedPage = '.' + $location.path().slice(1);
+               return selectedPage === page;
+            };
         }])
 })();
